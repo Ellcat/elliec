750fd7e19ea9c6edfc307cc644f62c8d96d9d1a0
vagrant <vagrant@precise32.(none)>
2015-09-05 07:08:32 +0000

    added show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,10 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+  
+    @post = Post.find(params[:id])
   end
+  
 
   # GET /posts/new
   def new
