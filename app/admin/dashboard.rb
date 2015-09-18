10b012973523f2cf5888cce9bd1ca6cfd5e9afd6
sethherr <seth.william.herr@gmail.com>
2015-09-18 10:13:30 -0500

    added functioning index columns

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,41 +3,25 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    #   span class: "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
-
     columns do
       column do
-        panel "top on column 1" do
-          Post.limit(10).collect do |post|
-            content_tag(:span, post.author)
+        panel "Admin users" do
+          table_for AdminUser.all do |user|
+            column :email
           end
         end
-        # some code
       end
-      # panel "second on column 1"
-      #   # some code
-      # end
-      # panel "last on column 1"
-      #   # some code
-      # end
+      column do 
+        panel "ALL your sweet posts" do
+          table_for Post.all do
+            column :author
+            column('body') { |b| b.body.truncate(200) }
+            column :published
+          end
+        end
+      end
     end
-  #   column do
-  #     panel "top on column 2"
-  #       # some code
-  #     end
-  #     panel "second on column 2"
-  #       # some code
-  #     end
-  #     panel "last on column 2"
-  #       # some code
-  #     end
-  #   end
-  end 
+  end
 end    
 
    
